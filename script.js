4239aee2823f41755225c051192d71e27ec64a18
Lincoln-Smith <525LSmith@frhsd.com>
2024-10-15 13:47:19 -0400

    day done

---
+++ script.js
@@ -0,0 +1,41 @@
+
+// Change for the year 1789
+function change1() {
+    document.getElementById("info").innerText = "Candy Crush!"
+    document.getElementById("placeholderImage").src = "imgs/trunk1.png";
+ }
+ 
+ // Change for the year 1957
+ function change2() {
+     document.getElementById("info").innerText = "Outer Space!"
+     document.getElementById("placeholderImage").src = "imgs/trunk2.png";
+ }
+ 
+ // Change for the year 1981
+ function change3() {
+     document.getElementById("info").innerText =  "Dr.Suess!"
+     document.getElementById("placeholderImage").src = "imgs/trunk3.png";
+ }
+ 
+ // Change for the year 1992
+ function change4() {
+ 
+     document.getElementById("info").innerText = "Barbie!"
+     document.getElementById("placeholderImage").src = "imgs/trunk4.png";
+ }
+ 
+ // Change for the year 2005
+ function change5() {
+ 
+     document.getElementById("info").innerText =   "Pirates!"
+     document.getElementById("placeholderImage").src = "imgs/trunk5.png";
+ 
+ }
+
+  // Change for the year 2005
+  function change6() {
+ 
+    document.getElementById("info").innerText =   "Christmas"
+    document.getElementById("placeholderImage").src = "imgs/trunk6.png";
+
+}
